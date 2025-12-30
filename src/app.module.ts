62d110582186d2a5896933822176a0911e49c6aa
Ekojoecovenant <covenantekojoe@gmail.com>
2025-12-30 19:11:40 +0000

    chore: register core domain modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookingModule } from './booking/booking.module';
+import { ResourceModule } from './resource/resource.module';
 
 @Module({
-  imports: [],
+  imports: [BookingModule, ResourceModule],
   controllers: [AppController],
   providers: [AppService],
 })
